ebb8d62381caeb519ad5892ff01e619588931950
Refaldy <refaldybanggana@gmail.com>
2024-03-08 11:11:50 +0700

    create header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "ReBlog",
+  url: "",
+  description: "learn markdown web",
+  author: "Refaldy Bagas Anggana",
+  link: {
+    twitter: "",
+    github: "https://github.com/refaldy04",
+    personalSite: "",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
