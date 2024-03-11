c3349012c2e6b9bb33097cc37acb9314f2e6b6a5
Refaldy <refaldybanggana@gmail.com>
2024-03-11 19:48:03 +0700

    SEO setup

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,6 +2,12 @@ import { posts } from "#site/content";
 import PostItem from "@/components/post-item";
 import QueryPagination from "@/components/query-pagination";
 import { sortPosts } from "@/lib/utils";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My blog",
+  description: "This is my blog about all things web development.",
+};
 
 const POSTS_PER_PAGE = 5;
 
